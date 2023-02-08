d607c6fc7205e3c79c2ecf375edbed6093d0f1bd
chavewain <dionisiochavez@gmail.com>
2023-02-08 12:58:48 -0400

    first commit

---
+++ src/About.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const About = () => {
+  return <div>Abaut...</div>
+}
+
+export default About
---
+++ src/Homepage.js
@@ -0,0 +1,9 @@
+import React, { PureComponent } from 'react'
+
+export class Homepage extends PureComponent {
+  render() {
+    return <div>Homepage...</div>
+  }
+}
+
+export default Homepage
